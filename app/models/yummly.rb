323dd367fe5b6f0661a323b5a017bd3531436faf
Lily Parmenter <parmenter.lily@gmail.com>
2015-09-02 17:19:20 -0500

    fixed space

--- app/models/yummly.rb
+++ app/models/yummly.rb
@@ -2,7 +2,7 @@ require 'unirest'
 
 class Yummly
 	def self.search(params = {})
-    response = Unirest.get("http://api.yummly.com/v1/api/recipes", headers:
+		response = Unirest.get("http://api.yummly.com/v1/api/recipes", headers:
                           { "Accept" => "application/json" },
                             parameters: { :_app_id => ENV['YUMMLY_APP_ID'].to_s,
                             :_app_key => ENV['YUMMLY_APP_KEY'].to_s , :allowedIngredient => params })
